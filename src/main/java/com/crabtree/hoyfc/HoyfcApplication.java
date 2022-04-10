72d43483f7c96a0cbc78a28e46adb4f6329d67e0
Matt Crabtree <66781848+mj-crabtree@users.noreply.github.com>
2022-04-10 14:55:30 +0100

    initial structure

---
+++ src/main/java/com/crabtree/hoyfc/HoyfcApplication.java
@@ -0,0 +1,13 @@
+package com.crabtree.hoyfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoyfcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoyfcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crabtree/hoyfc/HoyfcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crabtree.hoyfc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoyfcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
